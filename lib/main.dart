1842a79dc1d9b8707054cc8f6db3b516a156f91a
Vardaan Sharma <gokuldh@gmail.com>
2021-03-13 22:34:12 +0530

    Day 5
    Api Test using reqres.in

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,72 @@
 import 'package:flutter/material.dart';
-import 'strings.dart';
-import 'package:bootcamo_04_march/pages/choose_location.dart';
-import 'package:bootcamo_04_march/pages/home.dart';
-import 'package:bootcamo_04_march/pages/new_location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+//https://reqres.in/api/users?page=2
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/home',
-  routes: {
-    '/home': (context) => Home(),
-    '/location': (context) => ChooseLocation(),
-    '/newlocation': (context) => NewLocation(),
-  },
-
+  home: ApiTest(),
 ));
+
+class ApiTest extends StatefulWidget{
+  @override
+  _ApiTest createState() => _ApiTest();
+}
+
+class _ApiTest extends State<ApiTest>{
+
+  Map data;
+  List userData;
+
+  Future getData() async{
+    http.Response response = await http.get('https://reqres.in/api/users?page=2');
+    data = json.decode(response.body);
+    setState(() {
+      userData = data['data'];
+    });
+  }
+
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Api Test'),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: ListView.builder(
+          itemCount: userData == null? 0 : userData.length,
+          itemBuilder: (BuildContext context, index){
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(userData[index]['avatar']),
+                      radius: 30,
+                    ),
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text("${userData[index]['first_name']}, ${userData[index]['last_name']}"),
+                      ),
+                  ],
+                ),
+              ),
+            );
+          },
+      ),
+    );
+
+  }
+}
+
+// <, <=, a = 10, 100 == 100
 
 
 
--- lib/strings.dart
+++
@@ -1,6 +0,0 @@
-class Strings {
-
-  static const String titletext = "Flutter Bootcamp String";
-  static const String bodytext = "My name is Flutter";
-
-}
